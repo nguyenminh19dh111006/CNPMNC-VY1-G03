e32ad534c9187661774f6fb17593b0569092d559
nguyenminh19dh111006 <102563489+nguyenminh19dh111006@users.noreply.github.com>
2022-05-14 17:32:39 +0700

    ver 2

--- db.config.js
+++ db.config.js
@@ -1,10 +1,9 @@
 const config = {
-    user:'root',
-    password:'root',
+    user:'sa',
+    password:'loveTRINH2l',
     server:'157.245.207.242',
     database:'phongkhachsan',
     options: {
-        trustedconnection:true,
         enableArithAbort:true,
         instancename :'157.245.207.242',
         trustServerCertificate: true
